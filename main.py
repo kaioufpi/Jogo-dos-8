135cc84e96e7d47ec5a297990ad64a7aa6c5716a
KaioDelmondes <kaioregodearaujo@hotmail.com>
2017-10-08 18:27:44 -0300

    Dicionário de movimentos e teste para até  15 movimentos automatizado

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import no_matriz
 from indices import tab
 from operator import attrgetter as getatrb
 from time import time
-
+from movimentos import qtMov
 def construir_resposta(no):
 	if(no.pai is not None):
 		construir_resposta(no.pai)
@@ -18,16 +18,16 @@ def busca_largura(raiz):
 	i = 0
 	while borda:
 		no_atual = borda.popleft()
-		if not(no_atual.tabuleiro in explorado):
-			if no_atual.eh_solucao():
-				#construir_resposta(no_atual)
-				print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
-				return
-			else:
-				novos_filhos = no_atual.gerar_filhos()
-				borda.extend(novos_filhos)
-
-			explorado.extend(no_atual.tabuleiro)
+		#if not(no_atual.tabuleiro in explorado):
+		if no_atual.eh_solucao():
+			#construir_resposta(no_atual)
+			print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
+			return
+		else:
+			novos_filhos = no_atual.gerar_filhos()
+			borda.extend(novos_filhos)
+		
+		#explorado.extend(no_atual.tabuleiro)
 		i += 1
 
 def busca_heuristica(raiz):
@@ -38,17 +38,17 @@ def busca_heuristica(raiz):
 	borda.append(raiz)
 	while borda:
 		no_atual = borda.pop(0)
-		if not(no_atual.tabuleiro in explorado):
-			if no_atual.eh_solucao():
-				#construir_resposta(no_atual)
-				print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
-				return
-			else:
-				novos_filhos = no_atual.gerar_filhos()
-				borda.extend(novos_filhos)
-				borda.sort(key = lambda a : a.a_estrela())
-
-			explorado.extend(no_atual.tabuleiro)
+		#if not(no_atual.tabuleiro in explorado):
+		if no_atual.eh_solucao():
+			#construir_resposta(no_atual)
+			print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
+			return
+		else:
+			novos_filhos = no_atual.gerar_filhos()
+			borda.extend(novos_filhos)
+			borda.sort(key = lambda a : a.a_estrela())
+		
+		#explorado.extend(no_atual.tabuleiro)
 		i += 1
 
 def busca_gulosa(raiz):
@@ -68,24 +68,25 @@ def busca_gulosa(raiz):
 				novos_filhos = no_atual.gerar_filhos()
 				borda.extend(novos_filhos)
 				borda.sort(key = getatrb('fh'))
-
+			
 			explorado.extend(no_atual.tabuleiro)
 		i += 1
-	
-raiz = no_matriz.estado([[0, 1, 2], [5, 6, 3], [4, 7, 8]], None, 0, None)
-inicio = time()
-busca_heuristica(raiz)
-fim = time()
-print("Heuristica: ", fim - inicio, "\n\n")
 
-raiz = no_matriz.estado([[0, 1, 2], [5, 6, 3], [4, 7, 8]], None, 0, None)
-inicio = time()
-busca_largura(raiz)
-fim = time()
-print("Largura: ", fim - inicio, "\n\n")
+for i in range(15,16):
+	raiz = no_matriz.estado(qtMov[i], None, 0, None)
+	inicio = time()
+	busca_heuristica(raiz)
+	fim = time()
+	print("Heuristica: ", fim - inicio, "\n\n")
 
-raiz = no_matriz.estado([[0, 1, 2], [5, 6, 3], [4, 7, 8]], None, 0, None)
-inicio = time()
-busca_gulosa(raiz)
-fim = time()
-print("Gulosa: ", fim - inicio, "\n\n")
+	raiz = no_matriz.estado(qtMov[i], None, 0, None)
+	inicio = time()
+	busca_largura(raiz)
+	fim = time()
+	print("Largura: ", fim - inicio, "\n\n")
+
+	raiz = no_matriz.estado(qtMov[i], None, 0, None)
+	inicio = time()
+	busca_gulosa(raiz)
+	fim = time()
+	print("Gulosa: ", fim - inicio, "\n\n")
