0ead03d6f24692780c0dc0dff1e953d2a1e3ba43
renatoalmeidas <renatodealmeida_9@hotmail.com>
2017-10-07 21:50:37 -0300

    Funcionando setas

--- index.py
+++ index.py
@@ -2,7 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 from flask import render_template
 
-
+@app.route("/<vet>")
 @app.route("/")
-def index():
-    return render_template('index.html')
+def index(vet=None):
+    return render_template('index.html', vet=vet)
+
+'''@app.route("/largura")
+def largura():
+searchword = request.args.get('key', '')
+'''
