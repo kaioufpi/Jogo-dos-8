bd45e6cc0e4bb14c7c6e553f214ebf1db8f632b3
renatoalmeidas <renatodealmeida_9@hotmail.com>
2017-10-08 21:11:29 -0300

    Comunicação estabelecida

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, make_response, request, jsonify
+
+import json
+import main
+
 app = Flask(__name__)
 from flask import render_template
 
@@ -7,7 +11,21 @@ from flask import render_template
 def index(vet=None):
     return render_template('index.html', vet=vet)
 
-'''@app.route("/largura")
-def largura():
-searchword = request.args.get('key', '')
-'''
+@app.route("/resolve", methods=['GET','POST'])
+def resolve():
+    json=request.get_json()
+    alg=json[-1]
+    i=1
+    linha=0
+    coluna=0
+    vet=[[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+    while (i<26):
+        vet[linha][coluna]=int(json[i])
+        coluna+=1
+        if(coluna==3):
+            coluna=0
+            linha+=1
+        i=i+3
+    print(vet);
+    main.run(vet,alg)
+    return make_response("",200)
--- main.py
+++ main.py
@@ -5,87 +5,97 @@ from operator import attrgetter as getatrb
 from time import time
 
 def construir_resposta(no):
-	if(no.pai is not None):
-		construir_resposta(no.pai)
-	print(no.tabuleiro, "Altura: ", no.altura, "\n", ", Heuristica: ", no.fh, ", A*: ", no.a_estrela())
+    if(no.pai is not None):
+        construir_resposta(no.pai)
+    print(no.tabuleiro, "Altura: ", no.altura, "\n", ", Heuristica: ", no.fh, ", A*: ", no.a_estrela())
 
 
 def busca_largura(raiz):
-	explorado = []
-	borda = deque()
-	
-	borda.append(raiz)
-	i = 0
-	while borda:
-		no_atual = borda.popleft()
-		if not(no_atual.tabuleiro in explorado):
-			if no_atual.eh_solucao():
-				#construir_resposta(no_atual)
-				print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
-				return
-			else:
-				novos_filhos = no_atual.gerar_filhos()
-				borda.extend(novos_filhos)
+    explorado = []
+    borda = deque()
+    
+    borda.append(raiz)
+    i = 0
+    while borda:
+        no_atual = borda.popleft()
+        if not(no_atual.tabuleiro in explorado):
+            if no_atual.eh_solucao():
+                #construir_resposta(no_atual)
+                print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
+                return
+            else:
+                novos_filhos = no_atual.gerar_filhos()
+                borda.extend(novos_filhos)
 
-			explorado.extend(no_atual.tabuleiro)
-		i += 1
+            explorado.extend(no_atual.tabuleiro)
+        i += 1
 
 def busca_heuristica(raiz):
-	explorado = []
-	borda = []
-	
-	i = 0
-	borda.append(raiz)
-	while borda:
-		no_atual = borda.pop(0)
-		if not(no_atual.tabuleiro in explorado):
-			if no_atual.eh_solucao():
-				#construir_resposta(no_atual)
-				print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
-				return
-			else:
-				novos_filhos = no_atual.gerar_filhos()
-				borda.extend(novos_filhos)
-				borda.sort(key = lambda a : a.a_estrela())
+    explorado = []
+    borda = []
+    
+    i = 0
+    borda.append(raiz)
+    while borda:
+        no_atual = borda.pop(0)
+        if not(no_atual.tabuleiro in explorado):
+            if no_atual.eh_solucao():
+                #construir_resposta(no_atual)
+                print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
+                return
+            else:
+                novos_filhos = no_atual.gerar_filhos()
+                borda.extend(novos_filhos)
+                borda.sort(key = lambda a : a.a_estrela())
 
-			explorado.extend(no_atual.tabuleiro)
-		i += 1
+            explorado.extend(no_atual.tabuleiro)
+        i += 1
 
 def busca_gulosa(raiz):
-	explorado = []
-	borda = []
-	
-	i = 0
-	borda.append(raiz)
-	while borda:
-		no_atual = borda.pop(0)
-		if not(no_atual.tabuleiro in explorado):
-			if no_atual.eh_solucao():
-				#construir_resposta(no_atual)
-				print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
-				return
-			else:
-				novos_filhos = no_atual.gerar_filhos()
-				borda.extend(novos_filhos)
-				borda.sort(key = getatrb('fh'))
+    explorado = []
+    borda = []
+    
+    i = 0
+    borda.append(raiz)
+    while borda:
+        no_atual = borda.pop(0)
+        if not(no_atual.tabuleiro in explorado):
+            if no_atual.eh_solucao():
+                #construir_resposta(no_atual)
+                print("Posição do nó na fronteira: ", i , "Tamanho da borda: ", len(borda), "Altura: ", no_atual.altura)
+                return
+            else:
+                novos_filhos = no_atual.gerar_filhos()
+                borda.extend(novos_filhos)
+                borda.sort(key = getatrb('fh'))
 
-			explorado.extend(no_atual.tabuleiro)
-		i += 1
-	
-raiz = no_matriz.estado([[0, 1, 2], [5, 6, 3], [4, 7, 8]], None, 0, None)
-inicio = time()
-busca_heuristica(raiz)
-fim = time()
-print("Heuristica: ", fim - inicio, "\n\n")
+            explorado.extend(no_atual.tabuleiro)
+        i += 1
+    
+def run(vetor, alg):
 
-raiz = no_matriz.estado([[0, 1, 2], [5, 6, 3], [4, 7, 8]], None, 0, None)
-inicio = time()
-busca_largura(raiz)
-fim = time()
-print("Largura: ", fim - inicio, "\n\n")
 
-raiz = no_matriz.estado([[0, 1, 2], [5, 6, 3], [4, 7, 8]], None, 0, None)
-inicio = time()
-busca_gulosa(raiz)
-fim = time()
-print("Gulosa: ", fim - inicio, "\n\n")
+    if(alg=='1'):
+        raiz = no_matriz.estado(vetor, None, 0, None)
+        inicio = time()
+        busca_largura(raiz)
+        fim = time()
+        print("Largura: ", fim - inicio, "\n\n")
+
+    if(alg=='2'):
+        print("semestarfeito")
+    if(alg=='3'):
+        raiz = no_matriz.estado(vetor, None, 0, None)
+        inicio = time()
+        busca_heuristica(raiz)
+        fim = time()
+        print("Heuristica: ", fim - inicio, "\n\n")
+    if(alg=='4'):
+        raiz = no_matriz.estado(vetor, None, 0, None)
+        inicio = time()
+        busca_gulosa(raiz)
+        fim = time()
+        print("Gulosa: ", fim - inicio, "\n\n")
+
+
+
